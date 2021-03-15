48907e098d552b90a2b460dd92e78a2ee0d7dc91
amarfilali <medamarfilali@gmail.com>
2021-03-15 16:26:06 +0100

    Adding the QrDrqwer content

--- src/components/dashboard/dashboardLayout/qrDrawer/qrDrawerContent/QrDrawerContent.js
+++ src/components/dashboard/dashboardLayout/qrDrawer/qrDrawerContent/QrDrawerContent.js
@@ -1,5 +1,13 @@
+import { Typography } from "@material-ui/core";
+import { useStyles } from "./styleQrDrawerContent";
+
 const QrDrawerContent = () => {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.drawerContentContainer}>
+      <Typography variant="subtitle2">This is the content</Typography>
+    </div>
+  );
 };
 
 export default QrDrawerContent;
