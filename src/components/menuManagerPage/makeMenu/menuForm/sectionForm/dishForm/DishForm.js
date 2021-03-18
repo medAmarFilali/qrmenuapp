e03e764913e6006ba5a73b2abe7e2ed730c746c8
amarfilali <medamarfilali@gmail.com>
2021-03-18 13:04:47 +0100

    Added delete dish feature

--- src/components/menuManagerPage/makeMenu/menuForm/sectionForm/dishForm/DishForm.js
+++ src/components/menuManagerPage/makeMenu/menuForm/sectionForm/dishForm/DishForm.js
@@ -1,6 +1,7 @@
-import { TextField } from "@material-ui/core";
+import { TextField, Typography, IconButton } from "@material-ui/core";
 import { useStyles } from "./styleDishForm";
 import { produce } from "immer";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
 import clsx from "clsx";
 
 const DishForm = ({ id, sectionId, index, setMenuData, menuData, dish }) => {
@@ -54,35 +55,65 @@ const DishForm = ({ id, sectionId, index, setMenuData, menuData, dish }) => {
     );
   };
 
+  const handleDishDelete = (e, id) => {
+    setMenuData(
+      produce((draft) => {
+        const sectionIndex = draft.dishes.findIndex(
+          (item) => item.id === sectionId
+        );
+        if (sectionIndex !== -1) {
+          const dishIndex = draft.dishes[sectionIndex].items.findIndex(
+            (item) => item.id === id
+          );
+          if (dishIndex !== -1)
+            draft.dishes[sectionIndex].items.splice(dishIndex, 1);
+        }
+      })
+    );
+  };
+
   return (
     <div>
       <div className={classes.dishContainer}>
-        <TextField
-          label="Plat"
-          variant="outlined"
-          value={dish.dishName}
-          onChange={handleDishNameChange}
-          fullWidth
-          className={clsx(classes.textField, classes.dish)}
-        />
-        <TextField
-          label="Prix"
-          variant="outlined"
-          value={dish.dishPrice}
-          onChange={handleDishPriceChange}
-          fullWidth
-          className={clsx(classes.textField, classes.price)}
-        />
+        <div className={classes.dishHeader}>
+          <Typography variant="subtitle2">
+            {dish.dishName ? dish.dishName : "Plat"}
+          </Typography>
+          <svg width="120" height="2" xmlns="http://www.w3.org/2000/svg">
+            <path d="M 2 2 L 120 2" stroke="#333333" />
+          </svg>
+          <IconButton onClick={(e) => handleDishDelete(e, id)}>
+            <HighlightOffIcon className={classes.delIcon} />
+          </IconButton>
+        </div>
+        <div className={classes.dishDetails}>
+          <TextField
+            label="Plat"
+            variant="outlined"
+            value={dish.dishName}
+            onChange={handleDishNameChange}
+            fullWidth
+            className={clsx(classes.textField, classes.dish)}
+          />
+          <TextField
+            label="Prix"
+            variant="outlined"
+            value={dish.dishPrice}
+            onChange={handleDishPriceChange}
+            fullWidth
+            className={clsx(classes.textField, classes.price)}
+          />
+          <TextField
+            label="Description"
+            variant="outlined"
+            value={dish.dishDescription}
+            onChange={handleDishDescriptionChange}
+            fullWidth
+            className={classes.textFieldDescription}
+            rows={4}
+          />
+        </div>
       </div>
-      <TextField
-        label="Description"
-        variant="outlined"
-        value={dish.dishDescription}
-        onChange={handleDishDescriptionChange}
-        fullWidth
-        className={classes.textFieldDescription}
-        rows={4}
-      />
     </div>
   );
 };
--- src/components/menuManagerPage/makeMenu/menuForm/sectionForm/dishForm/styleDishForm.js
+++ src/components/menuManagerPage/makeMenu/menuForm/sectionForm/dishForm/styleDishForm.js
@@ -1,9 +1,30 @@
 import { makeStyles } from "@material-ui/core/styles";
+import { red, grey } from "@material-ui/core/colors";
 
 export const useStyles = makeStyles((theme) => ({
   dishContainer: {
     display: "flex",
+    flexDirection: "column",
+  },
+  dishHeader: {
+    display: "flex",
+    margin: theme.spacing(2, 0, 0, 0),
+    alignItems: "center",
     justifyContent: "space-between",
+    backgroundColor: grey[100],
+    borderRadius: theme.spacing(1),
+    "& h6": {
+      marginLeft: theme.spacing(1),
+    },
+  },
+  delIcon: {
+    fontSize: 20,
+    color: red[600],
+  },
+  dishDetails: {
+    display: "flex",
+    justifyContent: "space-between",
+    flexWrap: "wrap",
   },
   textField: {
     margin: theme.spacing(2, 0, 1, 0),
